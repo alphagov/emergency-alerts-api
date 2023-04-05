1801456cd165403d84eca51b5ad1a0bcc3e70d24
Jonathan Owens <115698235+jonathan-owens-gds@users.noreply.github.com>
2023-04-05 18:17:05 +0100

    EAS-940 Resolve config issues (#26)
    * roll back to correct template IDs; remove redundant config
    
    * remove config from dict
    
    * fix startup script

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -2,22 +2,12 @@
 
 echo "Start script executing for api.."
 
-# Query the fixed container agent IP address for credentials (search the AWS docs
-# for "Task IAM role" for more information about this endpoint)
-session_tokens=$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
-
-if [[ -z $CONTAINER_ROLE ]] || [[ "$CONTAINER_ROLE" == "" ]]; then
-  export CONTAINER_ROLE=$(echo $session_tokens | jq -j .RoleArn)
-fi
-
 function run_db_upgrade(){
   cd $API_DIR;
   . $VENV_API/bin/activate && flask db upgrade && flask db current
 }
 
 function configure_container_role(){
-  aws configure set role_arn $CONTAINER_ROLE
-  aws configure set credential_source EcsContainer
   aws configure set default.region eu-west-2
 }
 
