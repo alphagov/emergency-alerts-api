39e24b3097b52b265986a2633b1101ed562f3152
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-05-02 11:05:22 +0100

    fix type in config

--- app/config.py
+++ app/config.py
@@ -156,7 +156,7 @@ class Config(object):
         ],
         # this is overriden by the -Q command, but locally, we should read from all queues
         "task_queues": [Queue(queue, Exchange("default"), routing_key=queue) for queue in QueueNames.all_queues()],
-        "beath_scheduler": "celery.shedules:crontab",
+        "beat_scheduler": "celery.shedules:crontab",
         "beat_schedule": {
             "run-health-check": {
                 "task": "run-health-check",
