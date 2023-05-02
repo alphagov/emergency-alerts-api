5cb416ec27dbe63b0d1095b70a57339328f28817
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-05-02 13:42:06 +0100

    another attempt to configure CrontabScheduler

--- app/config.py
+++ app/config.py
@@ -156,7 +156,7 @@ class Config(object):
         ],
         # this is overriden by the -Q command, but locally, we should read from all queues
         "task_queues": [Queue(queue, Exchange("default"), routing_key=queue) for queue in QueueNames.all_queues()],
-        "beat_scheduler": "celery.schedulers.CrontabScheduler",
+        "beat_scheduler": "celery.schedules.CrontabScheduler",
         "task_acks_late": True,
         "beat_schedule": {
             "run-health-check": {
