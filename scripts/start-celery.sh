a6ba40620533b63dbd2cd5ed86d49dc9f992a2be
Lewis Stevens <lewis.stevens@digital.cabinet-office.gov.uk>
2023-05-24 13:40:11 +0100

    Adjusting the celery start script

--- scripts/start-celery.sh
+++ scripts/start-celery.sh
@@ -1,48 +1,21 @@
 #! /bin/sh
 
-echo "Start script executing for api.."
-
-function run_db_upgrade(){
-    cd $DIR_API;
-    . $VENV_API/bin/activate
-    
-    local head=$(flask db heads | tail -1)
-    local current=$(flask db current | tail -1)
-    
-    # Checking if the database upgrade has been previously been completed to avoid running multiple times.
-    if [[ $head != $current ]]; then
-        echo "Run DB migration"
-        flask db upgrade
-    else
-        echo "DB is up to date"
-    fi
-    
-    echo $(flask db current)
-}
+echo "Start script executing for celery beat.."
 
 function configure_container_role(){
-    aws configure set default.region eu-west-2
+  aws configure set default.region eu-west-2
 }
 
 function run_celery(){
-    cd $DIR_API;
-    . $VENV_API/bin/activate && make run-celery &
+  cd $DIR_API;
+  . $VENV_API/bin/activate && make run-celery &
 }
 
-function run_api(){
-    cd $DIR_API;
-    . $VENV_API/bin/activate && flask run -p 6011 --host=0.0.0.0
+function run_celery_beat(){
+  cd $DIR_API;
+  . $VENV_API/bin/activate && make run-celery-beat
 }
 
-if [[ ! -z $DEBUG ]]; then
-    echo "Starting in debug mode.."
-    while true; do echo 'Debug mode active..'; sleep 30; done
-else
-    configure_container_role
-    if [[ ! -z $MASTER_USERNAME ]]; then
-        run_db_upgrade
-    else
-        run_celery
-        run_api
-    fi
-fi
+configure_container_role
+run_celery
+run_celery_beat
