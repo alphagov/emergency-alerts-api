9fed5e49531493991b6abb730ffe8737ed8feac1
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-06-26 16:10:55 +0100

    tweak syntax of migration script

--- migrations/versions/0389_org_to_emergency_alerts.py
+++ migrations/versions/0389_org_to_emergency_alerts.py
@@ -5,7 +5,9 @@ Revises: 0388_populate_letter_branding
 Create Date: 2023-06-23 17:14:12:01.094412
 
 """
+
 from alembic import op
+from sqlalchemy.sql import text
 
 revision = "0389_org_to_emergency_alerts"
 down_revision = "0388_populate_letter_branding"
@@ -14,16 +16,17 @@ organisation_id = "38e4bf69-93b0-445d-acee-53ea53fe02df"
 
 
 def upgrade():
-    op.execute(
-        """
+    conn = op.get_bind()
+    conn.execute(
+        text("""
             UPDATE
                 organisation
             SET
                 name = 'Emergency Alerts'
             WHERE
-                name = :id;
-        """,
-        id=organisation_id,
+                id = :org_id
+        """),
+        {"org_id": organisation_id},
     )
 
 
