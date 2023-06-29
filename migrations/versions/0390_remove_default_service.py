86b233c7963e4b56ce7000dd59990c040243ef21
Lewis Stevens <lewis.stevens@digital.cabinet-office.gov.uk>
2023-06-29 12:45:58 +0100

    Removing default service remnants

---
+++ migrations/versions/0390_remove_default_service.py
@@ -0,0 +1,111 @@
+"""
+
+Revision ID: 0390_remove_default_service
+Revises: 0389_org_to_emergency_alerts
+Create Date: 2023-06-29 12:43:12:01.094412
+
+"""
+
+from alembic import op
+from sqlalchemy.sql import text
+
+revision = "0390_remove_default_service"
+down_revision = "0389_org_to_emergency_alerts"
+
+service_id = "d6aa2c68-a2d9-4437-ab19-3ae8eb202553"
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(
+        text("""
+            DELETE FROM
+                services
+            WHERE
+                id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                annual_billing
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                service_email_reply_to
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                service_permissions
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                service_sms_senders
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                services_history
+            WHERE
+                id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                templates
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                templates_history
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+    conn.execute(
+        text("""
+            DELETE FROM
+                user_to_service
+            WHERE
+                service_id = :id
+        """),
+        {"id": service_id},
+    )
+
+def downgrade():
+    pass
