658cca3631de4c95cbda5f4739a0abb46b903167
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-08-08 09:59:08 +0100

    fix lint error

--- app/verify/rest.py
+++ app/verify/rest.py
@@ -1,10 +1,16 @@
+import os
+
 from flask import Blueprint
 
 from app.dao.users_dao import get_code_for_user
+from app.errors import InvalidRequest
 
 verify_code_blueprint = Blueprint("verify", __name__)
 
 
 @verify_code_blueprint.route("/<uuid:user_id>", methods=["GET"])
 def get_latest_verify_code_for_user(user_id):
+    if os.environ.get("ENVIRONMENT") not in ["development", "preview"]:
+        raise InvalidRequest("Endpoint not found", status_code=404)
+
     return get_code_for_user(user_id)
