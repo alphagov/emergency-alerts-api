885fda8d016fd1fdcdcaccaf864a5164e6026f79
Jake <jake.wych@digital.cabinet-office.gov.uk>
2023-11-13 14:38:09 +0000

    changing naming convention

--- app/verify/rest.py
+++ app/verify/rest.py
@@ -10,7 +10,7 @@ verify_code_blueprint = Blueprint("verify", __name__)
 
 @verify_code_blueprint.route("/<uuid:user_id>", methods=["GET"])
 def get_latest_verify_code_for_user(user_id):
-    if os.environ.get("ENVIRONMENT") not in ["local", "development", "preview"]:
+    if os.environ.get("ENVIRONMENT") not in ["local", "dev", "preview"]:
         raise InvalidRequest("Endpoint not found", status_code=404)
 
     return get_code_for_user(user_id)
