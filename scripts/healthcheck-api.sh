e11cb0541df5decd3a3e69fd77307822788a505b
Lewis Stevens <lewis.stevens@digital.cabinet-office.gov.uk>
2023-12-07 09:59:19 +0000

    Updating the healthcheck script

---
+++ scripts/healthcheck-api.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+function check_status_endpoint(){
+  curl -f 0.0.0.0:6011/_api_status || exit 1
+}
+
+if [[ ! -z $DEBUG ]]; then
+  echo "Debug mode active.."
+else
+  check_status_endpoint
+fi
