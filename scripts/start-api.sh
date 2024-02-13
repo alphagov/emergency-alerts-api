a43a17e5ca8c6b37be54ee593d72cd5356164660
Adam Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2024-02-13 09:46:53 +0000

    Revert migrations

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -2,6 +2,24 @@
 
 echo "Start script executing for api.."
 
+function run_db_upgrade(){
+    cd $DIR_API;
+    . $VENV_API/bin/activate
+
+    local head=$(flask db heads | tail -1)
+    local current=$(flask db current | tail -1)
+
+    # Checking if the database upgrade has been previously been completed to avoid running multiple times.
+    if [[ $head != $current ]]; then
+        echo "Run DB migration"
+        flask db upgrade
+    else
+        echo "DB is up to date"
+    fi
+
+    echo $(flask db current)
+}
+
 function configure_container_role(){
     aws configure set default.region eu-west-2
 }
@@ -21,6 +39,10 @@ if [[ ! -z $DEBUG ]]; then
     while true; do echo 'Debug mode active..'; sleep 30; done
 else
     configure_container_role
-    run_celery
-    run_api
+    if [[ ! -z $MASTER_USERNAME ]]; then
+        run_db_upgrade
+    else
+        run_celery
+        run_api
+    fi
 fi
