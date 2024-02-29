ad36cbb01aa9ff4f04257bd2fd7987afb0eb876b
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-02-29 10:55:46 +0000

    use eas email addresses

--- tests/app/celery/test_scheduled_tasks.py
+++ tests/app/celery/test_scheduled_tasks.py
@@ -137,14 +137,14 @@ def test_delete_old_records_from_events_table(notify_db_session):
 def test_validate_functional_test_account_emails(notify_db_session):
     user1 = User(
         name="Test User 1",
-        email_address="preview-broadcast1@digital.cabinet-office.gov.uk",
+        email_address="emergency-alerts-tests+user1@digital.cabinet-office.gov.uk",
         password="password",
         auth_type="sms_auth",
         mobile_number="07700900000",
     )
     user2 = User(
         name="Test User 2",
-        email_address="preview-broadcast2@digital.cabinet-office.gov.uk",
+        email_address="emergency-alerts-tests+user2@digital.cabinet-office.gov.uk",
         password="password",
         auth_type="sms_auth",
         mobile_number="07700900000",
