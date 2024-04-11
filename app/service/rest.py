4c9e53528fddac9b4e8d0899f0d3d91e2c01b79a
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-04-11 17:52:34 +0100

    test

--- app/service/rest.py
+++ app/service/rest.py
@@ -1133,5 +1133,6 @@ def purge_services_created_by(user_id):
     except Exception as e:
         return jsonify(result="error", message=f"Unable to purge services created by user {user_id}: {e}"), 500
 
-    service_list = ",".join([x["id"] for x in services])
-    return jsonify({"message": f"Successfully purged services: {service_list}"}), 200
+    # service_list = ",".join([x["id"] for x in services])
+    # return jsonify({"message": f"Successfully purged services: {service_list}"}), 200
+    return "", 200
