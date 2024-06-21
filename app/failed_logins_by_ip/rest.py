abe39ad249dd6de3cd6a4ea9bbb57c6e1cfe6268
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-06-21 16:57:33 +0100

    Renamed files

--- app/failed_logins_by_ip/rest.py
+++ app/failed_logins_by_ip/rest.py
@@ -1,25 +1,26 @@
 import datetime
 from datetime import timedelta
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
-from app.dao.failed_login_count_by_ip_dao import (
+from app.dao.failed_logins_by_ip_dao import (
     dao_create_failed_login_for_ip,
     dao_get_latest_failed_login_by_ip,
 )
 from app.errors import InvalidRequest, register_errors
 
-failed_login_count_by_ip_blueprint = Blueprint(
+failed_logins_by_ip_blueprint = Blueprint(
     "failed_logins",
     __name__,
     url_prefix="/ip-failed-login-count",
 )
 
-register_errors(failed_login_count_by_ip_blueprint)
+register_errors(failed_logins_by_ip_blueprint)
 
 
-@failed_login_count_by_ip_blueprint.route("")
-def get_failed_login_count_by_ip(ip):
+@failed_logins_by_ip_blueprint.route("")
+def get_failed_logins_by_ip():
+    ip = request.remote_addr
     if not ip:
         errors = {"ip": ["Missing data for required field."]}
         raise InvalidRequest(errors, status_code=400)
@@ -27,8 +28,9 @@ def get_failed_login_count_by_ip(ip):
     return jsonify(data.serialize() if data else {}), 200
 
 
-@failed_login_count_by_ip_blueprint.route("")
-def add_failed_login_for_ip(ip):
+@failed_logins_by_ip_blueprint.route("")
+def create_failed_login_for_ip():
+    ip = request.remote_addr
     if not ip:
         errors = {"ip": ["Missing data for required field."]}
         raise InvalidRequest(errors, status_code=400)
@@ -36,8 +38,9 @@ def add_failed_login_for_ip(ip):
     return jsonify(data.serialize() if data else {}), 200
 
 
-@failed_login_count_by_ip_blueprint.route("")
-def check_failed_login_count_for_ip(ip):
+@failed_logins_by_ip_blueprint.route("")
+def check_failed_login_count_for_ip():
+    ip = request.remote_addr
     if not ip:
         errors = {"ip": ["Missing data for required field."]}
         raise InvalidRequest(errors, status_code=400)
@@ -51,4 +54,5 @@ def check_failed_login_count_for_ip(ip):
         delay_period = 120
     if current_time - failed_login_timestamp < timedelta(seconds=delay_period):
         errors = {"password": ["Incorrect password"]}
+        print('too soon!')
         raise InvalidRequest(errors, status_code=400)
