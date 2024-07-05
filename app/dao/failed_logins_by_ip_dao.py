c245df15e24f429ab9f882e9e7c73e1f28fec4a2
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-07-05 15:19:38 +0100

    Added time period argument to get_count_of_all_failed_logins_for_ip

--- app/dao/failed_logins_by_ip_dao.py
+++ app/dao/failed_logins_by_ip_dao.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 from sqlalchemy import desc
 
@@ -13,7 +13,7 @@ def dao_get_failed_logins():
 def dao_create_failed_login_for_ip(ip):
     data = FailedLoginCountByIP(
         ip=ip,
-        attempted_at=datetime.datetime.now(),
+        attempted_at=datetime.now(),
     )
     db.session.add(data)
     db.session.commit()
@@ -24,5 +24,10 @@ def dao_get_latest_failed_login_by_ip(ip):
     return FailedLoginCountByIP.query.filter_by(ip=ip).order_by(desc(FailedLoginCountByIP.attempted_at)).first() or None
 
 
-def dao_get_count_of_all_failed_logins_for_ip(ip):
-    return FailedLoginCountByIP.query.filter_by(ip=ip).count()
+def dao_get_count_of_all_failed_logins_for_ip(ip, time_period=1):
+    current_time = datetime.now()
+    return (
+        FailedLoginCountByIP.query.filter_by(ip=ip)
+        .filter(FailedLoginCountByIP.attempted_at >= current_time - timedelta(hours=time_period))
+        .count()
+    )
