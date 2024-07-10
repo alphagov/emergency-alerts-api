f03b01ffc828495b8a069bb5445c408885ce771b
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-07-10 15:22:54 +0100

    Created variables for test IPs and adjusted references

--- tests/app/dao/test_failed_login_dao.py
+++ tests/app/dao/test_failed_login_dao.py
@@ -8,8 +8,9 @@ from app.dao.failed_logins_by_ip_dao import (
     dao_get_latest_failed_login_by_ip,
 )
 from app.errors import InvalidRequest
-from app.failed_logins_by_ip.rest import check_throttle_for_ip
-from app.models import FailedLoginCountByIP
+from app.failed_logins_by_ip.rest import check_throttle_for_requester
+from app.models import FailedLogin
+from tests.app.conftest import test_ip_1
 from tests.app.db import create_failed_login
 
 
@@ -17,12 +18,12 @@ def test_get_failed_logins_returns_all_failed_logins(notify_db_session):
     """
     Creates 3 failed login records and checks that these are returned by dao_get_failed_logins.
     """
-    dao_create_failed_login_for_ip("192.0.2.15")
-    dao_create_failed_login_for_ip("192.0.2.15")
-    dao_create_failed_login_for_ip("192.0.2.15")
+    dao_create_failed_login_for_ip(test_ip_1)
+    dao_create_failed_login_for_ip(test_ip_1)
+    dao_create_failed_login_for_ip(test_ip_1)
     response = dao_get_failed_logins()
     assert len(response) == 3
-    assert {record.ip for record in response} == {"192.0.2.15"}
+    assert {record.ip for record in response} == {test_ip_1}
 
 
 def test_get_latest_failed_logins_returns_latest_failed_login(notify_db_session):
@@ -33,21 +34,23 @@ def test_get_latest_failed_logins_returns_latest_failed_login(notify_db_session)
     asserting that attempted_at value is the later one, thus it has returned the latest record.
 
     """
-    dao_create_failed_login_for_ip("192.0.2.15")
-    failed_login = create_failed_login(ip="192.0.2.15", attempted_at=datetime.now() + timedelta(seconds=30))
+    dao_create_failed_login_for_ip(test_ip_1)
+    failed_login = create_failed_login(ip=test_ip_1, attempted_at=datetime.now() + timedelta(seconds=30))
 
-    response = dao_get_latest_failed_login_by_ip("192.0.2.15")
+    response = dao_get_latest_failed_login_by_ip(test_ip_1)
     assert response.attempted_at and response.attempted_at == failed_login.attempted_at
 
 
-def test_get_failed_login_by_ip_returns_none_if_none_found(notify_db_session):
+def test_get_failed_logins_returns_none_if_none_found(notify_db_session):
     """
     Asserts that dao_get_failed_logins returns an empty list if no records have been added.
     """
     assert dao_get_failed_logins() == []
 
 
-def test_check_throttle_for_ip_raises_invalid_request_failed_login_too_soon(notify_db_session, admin_request, mocker):
+def test_check_throttle_for_requester_raises_invalid_request_failed_login_too_soon(
+    notify_db_session, admin_request, mocker
+):
     """
     Creates 3 failed login records, each with different values for attempted_at and asserts
     that dao_get_latest_failed_login_by_ip returns the record with the most recent attempted_at
@@ -60,21 +63,22 @@ def test_check_throttle_for_ip_raises_invalid_request_failed_login_too_soon(noti
 
     """
     attempted_at = None
-    mock_get_ip = mocker.patch("app.failed_logins_by_ip.rest.get_ip_address", return_value="127.0.0.1")
+
+    mock_get_ip = mocker.patch("app.failed_logins_by_ip.rest.get_ip_address", return_value=test_ip_1)
     mock_check_request_within_throttle_period = mocker.patch(
         "app.failed_logins_by_ip.rest.check_request_within_throttle_period"
     )
 
     for i in range(3):
         attempted_at = datetime.now() + timedelta(seconds=i * 10)
-        failed_login_1 = FailedLoginCountByIP(ip="127.0.0.1", attempted_at=attempted_at)
+        failed_login_1 = FailedLogin(ip=test_ip_1, attempted_at=attempted_at)
         notify_db_session.add(failed_login_1)
         notify_db_session.commit()
 
-    response = dao_get_latest_failed_login_by_ip("127.0.0.1")
-    assert response.ip and response.ip == "127.0.0.1" and response.attempted_at == attempted_at
+    response = dao_get_latest_failed_login_by_ip(test_ip_1)
+    assert response.ip and response.ip == test_ip_1 and response.attempted_at == attempted_at
     with pytest.raises(expected_exception=InvalidRequest) as e:
-        check_throttle_for_ip()
+        check_throttle_for_requester()
 
     mock_get_ip.assert_called_once()
     mock_check_request_within_throttle_period.assert_called_once()
--- tests/app/failed_logins_by_ip/test_rest.py
+++ tests/app/failed_logins_by_ip/test_rest.py
@@ -1,3 +1,4 @@
+from tests.app.conftest import test_ip_1, test_ip_2, test_ip_3
 from tests.app.test_utils import create_failed_login_for_test
 
 
@@ -6,39 +7,39 @@ def test_get_all_failed_logins_returns_all_records(notify_db_session, admin_requ
     Creates 2 failed login records and asserts that the response for get_all_failed_logins
     route has a length of 2 and that the IPs are the same as the ones just created.
     """
-    create_failed_login_for_test(notify_db_session, "192.0.2.15")
-    create_failed_login_for_test(notify_db_session, "192.0.2.30")
+    create_failed_login_for_test(notify_db_session, test_ip_1)
+    create_failed_login_for_test(notify_db_session, test_ip_2)
     response = admin_request.get(
         "failed_logins.get_all_failed_logins",
     )
     assert len(response) == 2
-    assert "ip" in response[0] and response[0]["ip"] == "192.0.2.15"
-    assert "ip" in response[1] and response[1]["ip"] == "192.0.2.30"
+    assert "ip" in response[0] and response[0]["ip"] == test_ip_1
+    assert "ip" in response[1] and response[1]["ip"] == test_ip_2
 
 
-def test_get_failed_login_by_ip_returns_empty_if_none_found(notify_db_session, admin_request, mocker):
+def test_get_failed_login_for_requester_returns_empty_if_none_found(notify_db_session, admin_request, mocker):
     """
-    Asserts that the response for the get_failed_login_by_ip is empty when no failed login attempts
+    Asserts that the response for the get_failed_login_for_requester is empty when no failed login attempts
     have been created.
     """
-    response = admin_request.get("failed_logins.get_failed_login_by_ip")
+    response = admin_request.get("failed_logins.get_failed_login_for_requester")
     assert response == {}
 
 
-def test_get_failed_login_by_ip_returns_only_failed_logins_for_ip(notify_db_session, admin_request, mocker):
+def test_get_failed_login_for_requester_returns_only_failed_logins_for_ip(notify_db_session, admin_request, mocker):
     """
     Creates a failed login record with a specific IP and then asserts that the response
-    from get_failed_login_by_ip route is empty as the IP isn't the same as test IP and thus
+    from get_failed_login_for_requester route is empty as the IP isn't the same as test IP and thus
     unable to fetch it.
     Then creates a failed login record with test IP and asserts that it is returned in the response
-    from get_failed_login_by_ip route.
+    from get_failed_login_for_requester route.
     """
-    create_failed_login_for_test(notify_db_session, "192.0.2.30")
+    create_failed_login_for_test(notify_db_session, test_ip_2)
 
-    response = admin_request.get("failed_logins.get_failed_login_by_ip")
+    response = admin_request.get("failed_logins.get_failed_login_for_requester")
     assert response == {}
 
-    create_failed_login_for_test(notify_db_session, "127.0.0.1")
+    create_failed_login_for_test(notify_db_session, test_ip_3)
 
-    response = admin_request.get("failed_logins.get_failed_login_by_ip")
-    assert "ip" in response and response["ip"] == "127.0.0.1"
+    response = admin_request.get("failed_logins.get_failed_login_for_requester")
+    assert "ip" in response and response["ip"] == test_ip_3
