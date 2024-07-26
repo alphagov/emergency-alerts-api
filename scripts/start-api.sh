bfb9982bfb32c5944a2dbc32a85b20b5d6016d1b
Lewis Stevens <lewis.stevens@digital.cabinet-office.gov.uk>
2024-07-26 11:00:46 +0100

    Updating the dimensions

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -12,21 +12,19 @@ function put_metric_data(){
         exit 1;
     fi
 
-    dimension="Status=$2"
-    if [[ $1 == "Migrations" ]]; then
-        if [[ -z $START_TIME ]]; then
-            echo "START_TIME is not provided and required."
-            exit 1;
-        fi
-
-        if [[ -z $PIPELINE_RUN_ID ]]; then
-            echo "PIPELINE_RUN_ID is not provided and required."
-            exit 1;
-        fi
-
-        # For future we should add a version e.g. Version=1112_test
-        dimension="PipelineRunId=$PIPELINE_RUN_ID,StartTime=$START_TIME,Status=success"
+    if [[ -z $START_TIME ]]; then
+        echo "START_TIME is not provided and required."
+        exit 1;
     fi
+
+    if [[ -z $PIPELINE_RUN_ID ]]; then
+        echo "PIPELINE_RUN_ID is not provided and required."
+        exit 1;
+    fi
+
+    # For future we should add a version e.g. Version=1112_test
+    dimension="PipelineRunId=$PIPELINE_RUN_ID,StartTime=$START_TIME,Status=$2"
+
 
     echo "Putting metric $1[$SERVICE_ACTION] with dimension: $dimension"
     aws cloudwatch put-metric-data \
