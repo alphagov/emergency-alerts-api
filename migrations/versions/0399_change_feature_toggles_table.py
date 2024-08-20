9217d89fd409138d3ab15c11f8c425044c6ddfcd
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-08-20 15:16:15 +0100

    Added migrations version to change text in feature_toggles_table

---
+++ migrations/versions/0399_change_feature_toggles_table.py
@@ -0,0 +1,34 @@
+"""
+
+Revision ID: 0399_change_feature_toggles_table
+Revises: 0398_add_failed_logins_table
+Create Date: 2024-08-20 15:31:10.657408
+
+"""
+
+from alembic import op
+
+revision = "0399_change_feature_toggles_table"
+down_revision = "0398_add_failed_logins_table"
+
+
+def upgrade():
+    op.execute("DELETE FROM feature_toggles where name = 'service_is_not_live'")
+    op.execute(
+        "INSERT INTO feature_toggles \
+            (name, is_enabled, display_html) \
+        VALUES \
+            ('service_is_not_live', false, \
+                'This is a trial mode service......')"
+    )
+
+
+def downgrade():
+    op.execute("DELETE FROM feature_toggles where name = 'service_is_not_live'")
+    op.execute(
+        "INSERT INTO feature_toggles \
+            (name, is_enabled, display_html) \
+        VALUES \
+            ('service_is_not_live', false, \
+                'This service is currently not live. You cannot send alerts out to the public.')"
+    )
