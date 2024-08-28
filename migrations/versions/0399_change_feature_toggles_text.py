9249863b2c04b437289fda5fa00d94477414b300
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-08-28 17:21:02 +0100

    Updated text in migration version query

--- migrations/versions/0399_change_feature_toggles_text.py
+++ migrations/versions/0399_change_feature_toggles_text.py
@@ -19,7 +19,7 @@ def upgrade():
             (name, is_enabled, display_html) \
         VALUES \
             ('service_is_not_live', false, \
-                'This is a trial mode service......')"
+                'This account is in trial mode. You cannot send alerts out to the public.')"
     )
 
 
