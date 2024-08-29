a4ac29082e0b5949a231cb5a1cde4b4777c3da9f
leylayaltiligil <151185790+leylayaltiligil@users.noreply.github.com>
2024-08-29 11:51:42 +0100

    EAS-2295 Added migrations version to change text in feature_toggles_table (#145)
    * Added migrations version to change text in feature_toggles_table
    
    * Formatted the version
    
    * Reduced version name to not exceed 32 characters
    
    * Updated text in migration version query
    
    * Changed text for banner
    
    * Changed text to reflect current banner text, for downgrade function

---
+++ migrations/versions/0399_change_feature_toggles_text.py
@@ -0,0 +1,34 @@
+"""
+
+Revision ID: 0399_change_feature_toggles_text
+Revises: 0398_add_failed_logins_table
+Create Date: 2024-08-20 15:31:10.657408
+
+"""
+
+from alembic import op
+
+revision = "0399_change_feature_toggles_text"
+down_revision = "0398_add_failed_logins_table"
+
+
+def upgrade():
+    op.execute("DELETE FROM feature_toggles where name = 'service_is_not_live'")
+    op.execute(
+        "INSERT INTO feature_toggles \
+            (name, is_enabled, display_html) \
+        VALUES \
+            ('service_is_not_live', false, \
+                'This environment is not production. You cannot send alerts out to the public.')"
+    )
+
+
+def downgrade():
+    op.execute("DELETE FROM feature_toggles where name = 'service_is_not_live'")
+    op.execute(
+        "INSERT INTO feature_toggles \
+            (name, is_enabled, display_html) \
+        VALUES \
+            ('service_is_not_live', false, \
+                'This is not live. You cannot send alerts out to the public.')"
+    )
