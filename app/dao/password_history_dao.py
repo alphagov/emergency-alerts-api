d37419130f1fdeff7a2ddeb021858724d2e85a05
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-10-04 15:35:02 +0100

    Removed unnecessary dao function

--- app/dao/password_history_dao.py
+++ app/dao/password_history_dao.py
@@ -19,10 +19,5 @@ def dao_get_count_of_all_historic_passwords_for_user(user_id):
     return PasswordHistory.query.filter_by(user_id=user_id).count()
 
 
-def dao_delete_all_failed_logins_for_user_id(user_id):
-    PasswordHistory.query.filter_by(user_id=user_id).delete()
-    db.session.commit()
-
-
 def dao_get_all_passwords_for_user(user_id):
     return PasswordHistory.query.filter_by(user_id=user_id).all()
