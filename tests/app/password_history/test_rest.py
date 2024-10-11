7cd5d5e9bf158b337f05a7fc0d655de2f9216145
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-10-11 10:23:22 +0100

    Added test for password_history rest functions

---
+++ tests/app/password_history/test_rest.py
@@ -0,0 +1,14 @@
+import uuid
+from app.password_history.rest import (
+    add_password_for_user,
+    is_password_for_user_already_in_table,
+)
+
+test_user = uuid.uuid4()
+
+
+def test_is_password_for_user_already_in_table(notify_db_session, admin_request, mocker):
+    add_password_for_user(test_user, "test")
+    add_password_for_user(test_user, "test2")
+    assert is_password_for_user_already_in_table(test_user, "test") is True
+
