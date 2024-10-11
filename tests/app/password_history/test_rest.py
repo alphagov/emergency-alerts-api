2d5a666b7666ea8a907102d8a87e28aca60ae026
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-10-11 10:33:34 +0100

    Applied formatting changes

--- tests/app/password_history/test_rest.py
+++ tests/app/password_history/test_rest.py
@@ -1,4 +1,5 @@
 import uuid
+
 from app.password_history.rest import (
     add_password_for_user,
     is_password_for_user_already_in_table,
@@ -11,4 +12,3 @@ def test_is_password_for_user_already_in_table(notify_db_session, admin_request,
     add_password_for_user(test_user, "test")
     add_password_for_user(test_user, "test2")
     assert is_password_for_user_already_in_table(test_user, "test") is True
-
