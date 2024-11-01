39da9799077909f64ecba24161e77b21d1c5b806
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-11-01 08:13:00 +0000

    reformat

--- migrations/versions/0402_drop_deprecated_tables.py
+++ migrations/versions/0402_drop_deprecated_tables.py
@@ -28,7 +28,7 @@ def upgrade():
     op.drop_column("templates_history", "service_letter_contact_id")
 
     op.drop_table("service_letter_contacts")
-    op.drop_table("service_whitelist")    
+    op.drop_table("service_whitelist")
     pass
 
 
