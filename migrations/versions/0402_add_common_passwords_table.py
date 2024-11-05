88758bc139142fc5fee5c7cbc7c4164b9adc2ad6
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-11-05 10:42:29 +0000

    Added s3 actions to migration file

--- migrations/versions/0402_add_common_passwords_table.py
+++ migrations/versions/0402_add_common_passwords_table.py
@@ -6,6 +6,11 @@ Create Date: 2024-10-31 11:33:35
 
 """
 
+from app.aws.s3 import file_exists
+from app.utils import is_local_host
+import boto3
+import uuid
+from flask import current_app
 import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects import postgresql
@@ -14,8 +19,13 @@ revision = "0402_add_common_passwords_table"
 down_revision = "0401_drop_job_tables"
 
 
+s3 = boto3.client('s3')
+passwords_file = current_app.config["COMMON_PASSWORDS_FILEPATH"]
+target_filepath = '/tmp/passwords.txt'
+
+
 def upgrade():
-    op.create_table(
+    common_passwords_table = op.create_table(
         "common_passwords",
         sa.Column("id", postgresql.UUID(as_uuid=True), nullable=False),
         sa.Column("password", sa.String(), nullable=False),
@@ -28,7 +38,29 @@ def upgrade():
         ["password"],
         unique=True,
     )
+    if is_local_host():
+        with open(passwords_file, 'r') as file:
+            passwords = file.readlines()
+        data = [(str(uuid.uuid4()), password.strip()) for password in passwords if password != ""]
+        op.bulk_insert(common_passwords_table, [{'id': row[0], 'password': row[1]} for row in data])
+    elif file_exists(current_app.config["COMMON_PASSWORDS_BUCKET_NAME"], passwords_file):
+        print('File exists')
+        download_file_from_s3()
+        with open(target_filepath, 'r') as file:
+            passwords = file.readlines()
+        if passwords:
+            data = [(str(uuid.uuid4()), password.strip()) for password in passwords if password != ""]
+            op.bulk_insert(common_passwords_table, [{'id': row[0], 'password': row[1]} for row in data])
+        else:
+            print('Passwords file was empty')
+    else:
+        print('No common passwords file found')
 
 
 def downgrade():
     op.drop_table("common_passwords")
+
+
+def download_file_from_s3():
+    s3.download_file(current_app.config["COMMON_PASSWORDS_BUCKET_NAME"], passwords_file, target_filepath)
+
