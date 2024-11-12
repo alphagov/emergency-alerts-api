0a356100730b84ac5e963746a69f2dbc50c86d3d
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-11-12 12:36:51 +0000

    Added create common password dao function

--- app/dao/common_passwords_dao.py
+++ app/dao/common_passwords_dao.py
@@ -1,5 +1,13 @@
+from app import db
 from app.models import CommonPasswords
 
 
 def dao_get_common_password_by_password(password):
+    return CommonPasswords.query.filter_by(password=password).first()
+
+
+def dao_create_common_password(password):
+    data = CommonPasswords(password=password)
+    db.session.add(data)
+    db.session.commit()
     return CommonPasswords.query.filter_by(password=password).first()
