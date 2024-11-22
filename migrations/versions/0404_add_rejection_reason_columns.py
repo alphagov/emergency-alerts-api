9be5936fadc946711c4eceffc610cf2fbf39bb4b
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-11-22 11:40:01 +0000

    Added migration version for new broadcast_message rejection columns

---
+++ migrations/versions/0404_add_rejection_reason_columns.py
@@ -0,0 +1,24 @@
+"""
+
+Revision ID: 0404_add_rejection_reason_columns
+Revises: 0403_add_common_passwords_table
+Create Date: 2024-11-22 10:08:10
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "0404_add_rejection_reason_columns"
+down_revision = "0403_add_common_passwords_table"
+
+
+def upgrade():
+    op.add_column("broadcast_message", sa.Column("rejection_reason", sa.Text(), nullable=True))
+    op.add_column("broadcast_message", sa.Column("rejected_by_id", postgresql.UUID(as_uuid=True), nullable=True))
+
+
+def downgrade():
+    op.drop_column("broadcast_message", "rejection_reason")
+    op.drop_column("broadcast_message", "rejected_by_id")
