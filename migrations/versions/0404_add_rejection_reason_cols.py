b3471e3b2f832339e54fef9698f1354fbe061a9b
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-11-26 17:43:16 +0000

    Added rejected_at column to table

--- migrations/versions/0404_add_rejection_reason_cols.py
+++ migrations/versions/0404_add_rejection_reason_cols.py
@@ -17,8 +17,10 @@ down_revision = "0403_add_common_passwords_table"
 def upgrade():
     op.add_column("broadcast_message", sa.Column("rejection_reason", sa.Text(), nullable=True))
     op.add_column("broadcast_message", sa.Column("rejected_by_id", postgresql.UUID(as_uuid=True), nullable=True))
+    op.add_column("broadcast_message", sa.Column("rejected_at", sa.DateTime(), nullable=True))
 
 
 def downgrade():
     op.drop_column("broadcast_message", "rejection_reason")
     op.drop_column("broadcast_message", "rejected_by_id")
+    op.drop_column("broadcast_message", "rejected_at")
