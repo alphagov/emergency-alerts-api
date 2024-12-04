a6dc3053430122a24718525b31cd484e37dd72fe
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-12-04 18:52:13 +0000

    Renamed revision

--- migrations/versions/0406_add_rejection_reason_cols.py
+++ migrations/versions/0406_add_rejection_reason_cols.py
@@ -1,7 +1,7 @@
 """
 
-Revision ID: 0405_add_rejection_reason_cols
-Revises: 0403_add_common_passwords_table
+Revision ID: 0406_add_rejection_reason_cols
+Revises: 0405_drop_notify_org_fields
 Create Date: 2024-11-22 10:08:10
 
 """
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects import postgresql
 
-revision = "0405_add_rejection_reason_cols"
-down_revision = "0404_drop_constraints"
+revision = "0406_add_rejection_reason_cols"
+down_revision = "0405_drop_notify_org_fields"
 
 
 def upgrade():
