fb070ef4ed1c6299458108cefc6443035ae2e2e2
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-12-12 13:51:16 +0000

    Added foreign key contraint for rejected_by_id

--- migrations/versions/0408_add_rejection_reason_cols.py
+++ migrations/versions/0408_add_rejection_reason_cols.py
@@ -18,9 +18,17 @@ def upgrade():
     op.add_column("broadcast_message", sa.Column("rejection_reason", sa.Text(), nullable=True))
     op.add_column("broadcast_message", sa.Column("rejected_by_id", postgresql.UUID(as_uuid=True), nullable=True))
     op.add_column("broadcast_message", sa.Column("rejected_at", sa.DateTime(), nullable=True))
+    op.create_foreign_key(
+        "broadcast_message_rejected_by_id_fkey",
+        "broadcast_message",
+        "users",
+        ["rejected_by_id"],
+        ["id"],
+    )
 
 
 def downgrade():
+    op.drop_constraint("broadcast_message_rejected_by_id_fkey", "broadcast_message", type_="foreignkey")
     op.drop_column("broadcast_message", "rejection_reason")
     op.drop_column("broadcast_message", "rejected_by_id")
     op.drop_column("broadcast_message", "rejected_at")
