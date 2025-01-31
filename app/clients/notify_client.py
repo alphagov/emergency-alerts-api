883c245c1335d8c8c76ad99e6eb195e11bf3b319
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-01-31 09:53:19 +0000

    revert notify_client changes

--- app/clients/notify_client.py
+++ app/clients/notify_client.py
@@ -19,7 +19,6 @@ __notify.client = None
 def get_notify_client():
     if is_local_host():
         return
-
     try:
         api_key = os.environ.get("NOTIFY_CLIENT_API_KEY", "empty_key")
         __notify.client = NotificationsAPIClient(api_key)
@@ -32,66 +31,36 @@ def get_notify_client():
 def get_notify_template(id):
     if is_local_host():
         return
-
     if __notify.client is None:
         get_notify_client()
-
     response = __notify.client.get_template(id)
-
     if response is None:
         raise APIError(f"Template {id} not found")
-
     return response
 
 
 def notify_send(notification):
     if is_local_host():
         return
-
     if __notify.client is None:
         get_notify_client()
 
     try:
         response = None
-        msg_type = notification["type"]
-        if msg_type == SMS_TYPE:
+        if notification["type"] == SMS_TYPE:
             response = __notify.client.send_sms_notification(
                 phone_number=notification["recipient"],
                 template_id=notification["template_id"],
                 personalisation=notification["personalisation"],
             )
-        if msg_type == EMAIL_TYPE:
+        if notification["type"] == EMAIL_TYPE:
             response = __notify.client.send_email_notification(
                 email_address=notification["recipient"],
                 template_id=notification["template_id"],
                 personalisation=notification["personalisation"],
                 email_reply_to_id=notification["reply_to"],
             )
-        if response is None:
-            current_app.logger.warning(
-                "Empty response from Notify API",
-                extra={
-                    "python_module": __name__,
-                    "notification": notification,
-                },
-            )
-        else:
-            current_app.logger.info(
-                f"Success sending {msg_type} to Notify API",
-                extra={
-                    "python_module": __name__,
-                    "notification": notification,
-                    "response": response,
-                },
-            )
     except Exception:
-        current_app.logger.exception(
-            "Error sending notification",
-            extra={
-                "python_module": __name__,
-                "notification": notification,
-                "response": response,
-            },
-        )
+        current_app.logger.exception("Error sending notification", extra={"python_module": __name__})
 
     return response
