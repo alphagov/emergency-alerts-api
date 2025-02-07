47709753389db7938904c929235cdcc61d712c74
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-02-07 15:42:06 +0000

    Added broadcast_message_history rest functions

---
+++ app/broadcast_message_history/rest.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, jsonify
+
+from app.dao.broadcast_message_history_dao import (
+    dao_create_broadcast_message_version,
+    dao_get_broadcast_message_version_by_id_and_service_id,
+    dao_get_broadcast_message_versions,
+)
+from app.errors import register_errors
+from app.schemas import broadcast_message_history_schema
+
+broadcast_message_history_blueprint = Blueprint(
+    "broadcast_message_history", __name__, url_prefix="/service/<uuid:service_id>/broadcast-message-history"
+)
+register_errors(broadcast_message_history_blueprint)
+
+
+@broadcast_message_history_blueprint.route("/<uuid:broadcast_message_id>/version/<int:version>")
+def get_broadcast_message_version(service_id, broadcast_message_id, version):
+    data = broadcast_message_history_schema.dump(
+        dao_get_broadcast_message_version_by_id_and_service_id(
+            broadcast_message_id=broadcast_message_id, service_id=service_id, version=version
+        )
+    )
+    return jsonify(data)
+
+
+@broadcast_message_history_blueprint.route("/<uuid:broadcast_message_id>/versions")
+def get_broadcast_message_versions(service_id, broadcast_message_id):
+    broadcast_messages = [
+        {
+            **message.serialize(),
+            "created_by": creatd_by or None,
+        }
+        for message, creatd_by in dao_get_broadcast_message_versions(
+            service_id=service_id, broadcast_message_id=broadcast_message_id
+        )
+    ]
+    return jsonify(broadcast_messages)
+
+
+def create_broadcast_message_version(broadcast_message_id, service_id):
+    dao_create_broadcast_message_version(broadcast_message_id, service_id)
