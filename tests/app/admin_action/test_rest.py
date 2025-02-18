76576aa8a64c3c751bc143370856e4aaf21dd6f8
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-02-18 10:04:48 +0000

    Add create test

--- tests/app/admin_action/test_rest.py
+++ tests/app/admin_action/test_rest.py
@@ -1,5 +1,38 @@
-from app.models import ADMIN_INVITE_USER
+import uuid
+
+from app.models import ADMIN_INVITE_USER, ADMIN_STATUS_PENDING, AdminAction
 from tests.app.db import create_admin_action
+
+
+def test_create_admin_action(admin_request, service_factory, sample_user, notify_db_session):
+    service = service_factory.get("test")
+
+    action_data = {
+        "email_address": "pending@test.com",
+        "permissions": ["create_broadcasts"],
+        "login_authentication": "email_auth",
+        "folder_permissions": [str(uuid.uuid4())],
+    }
+
+    action = {
+        "service_id": str(service.id),
+        "action_type": ADMIN_INVITE_USER,
+        "action_data": action_data,
+        "created_by": str(sample_user.id),
+    }
+
+    admin_request.post("admin_action.create_admin_action", action, _expected_status=201)
+
+    actions = AdminAction.query.all()
+    assert len(actions) == 1
+    assert actions[0].service_id == service.id
+    assert actions[0].action_type == ADMIN_INVITE_USER
+    assert actions[0].action_data == action_data
+    assert actions[0].created_by_id == sample_user.id
+    assert actions[0].created_at is not None
+    assert actions[0].status == ADMIN_STATUS_PENDING
+    assert actions[0].reviewed_by is None
+    assert actions[0].reviewed_at is None
 
 
 def test_get_all_pending_admin_actions(admin_request, service_factory, sample_user, notify_db_session):
