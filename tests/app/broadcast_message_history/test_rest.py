f735517ead233ca68496c63d8faa2bd4c80a1cef
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-03-03 23:40:21 +0000

    Adds rest tests for broadcast_message_history

---
+++ tests/app/broadcast_message_history/test_rest.py
@@ -0,0 +1,51 @@
+import uuid
+
+from freezegun import freeze_time
+
+from tests.app.db import create_broadcast_message_version
+
+
+def test_get_broadcast_message_version(admin_request, sample_service):
+    id = uuid.uuid4()
+
+    with freeze_time("2020-01-01 11:00"):
+        bmv3 = create_broadcast_message_version(service_id=sample_service.id, id=id, version=3)
+    with freeze_time("2020-01-01 12:00"):
+        create_broadcast_message_version(service_id=sample_service.id, id=id, version=1)
+    with freeze_time("2020-01-01 13:00"):
+        create_broadcast_message_version(service_id=sample_service.id, id=id, version=2)
+
+    response = admin_request.get(
+        "broadcast_message_history.get_broadcast_message_version",
+        service_id=str(sample_service.id),
+        broadcast_message_id=id,
+        version=3,
+        _expected_status=200,
+    )
+
+    assert response["id"] == str(bmv3.id)
+    assert response["areas"] == bmv3.areas
+    assert response["content"] == bmv3.content
+    assert response["reference"] == bmv3.reference
+
+
+@freeze_time("2020-01-01")
+def test_get_broadcast_message_versions(admin_request, sample_service):
+    id = uuid.uuid4()
+    with freeze_time("2020-01-01 12:00"):
+        bmv1 = create_broadcast_message_version(service_id=sample_service.id, id=id, version=1)
+    with freeze_time("2020-01-01 13:00"):
+        bmv2 = create_broadcast_message_version(service_id=sample_service.id, id=id, version=2)
+
+    response = admin_request.get(
+        "broadcast_message_history.get_broadcast_message_versions",
+        service_id=str(sample_service.id),
+        broadcast_message_id=id,
+        _expected_status=200,
+    )
+
+    assert len(response) == 2
+    assert response[0]["id"] == str(bmv1.id)
+    assert response[1]["id"] == str(bmv2.id)
+    assert response[0]["content"] == bmv1.content == "Test Broadcast Content"
+    assert response[1]["content"] == bmv2.content
