c4b1c07eaa02170fba88fa98edd893690008f938
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-03-13 13:43:09 +0000

    Adds duration to test

--- tests/app/broadcast_message_history/test_rest.py
+++ tests/app/broadcast_message_history/test_rest.py
@@ -2,6 +2,7 @@ import uuid
 
 from freezegun import freeze_time
 
+from app.utils import get_interval_seconds_or_none
 from tests.app.db import create_broadcast_message_version
 
 
@@ -9,11 +10,15 @@ def test_get_broadcast_message_version(admin_request, sample_service):
     id = uuid.uuid4()
 
     with freeze_time("2020-01-01 11:00"):
-        bmv3 = create_broadcast_message_version(service_id=sample_service.id, id=id, version=3, content="Test 3")
+        bmv3 = create_broadcast_message_version(
+            service_id=sample_service.id, id=id, version=3, content="Test 3", duration="04:00:00"
+        )
     with freeze_time("2020-01-01 12:00"):
         create_broadcast_message_version(service_id=sample_service.id, id=id, version=1, content="Test 1")
     with freeze_time("2020-01-01 13:00"):
-        create_broadcast_message_version(service_id=sample_service.id, id=id, version=2, content="Test 2")
+        create_broadcast_message_version(
+            service_id=sample_service.id, id=id, version=2, content="Test 2", duration="01:00:00"
+        )
 
     response = admin_request.get(
         "broadcast_message_history.get_broadcast_message_version",
@@ -27,6 +32,7 @@ def test_get_broadcast_message_version(admin_request, sample_service):
     assert response["areas"] == bmv3.areas
     assert response["content"] == bmv3.content
     assert response["reference"] == bmv3.reference
+    assert response["duration"] == get_interval_seconds_or_none(bmv3.duration)
 
 
 @freeze_time("2020-01-01")
