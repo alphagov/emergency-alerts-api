fa56c9cf35f1936dd06aafd1a6780dcd06222b49
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-03-17 12:16:13 +0000

    Add endpoint to purge test admin actions

--- app/dao/admin_action_dao.py
+++ app/dao/admin_action_dao.py
@@ -9,3 +9,12 @@ def dao_get_pending_admin_actions() -> list[AdminAction]:
 
 def dao_get_admin_action_by_id(action_id: str) -> AdminAction:
     return AdminAction.query.filter(AdminAction.id == action_id).one()
+
+
+def dao_get_all_admin_actions_by_user_id(user_id) -> list[AdminAction]:
+    return AdminAction.query.filter_by(created_by_id=user_id).all()
+
+
+def dao_delete_admin_action_by_id(action_id):
+    """Delete an admin action - not just reject it (for tests)"""
+    return AdminAction.query.filter_by(id=action_id).delete()
