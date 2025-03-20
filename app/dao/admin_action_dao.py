5df1d933553705dedc35c5872794739b342576ff
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-03-20 10:17:49 +0000

    Add autocommit so actions actually get deleted...

--- app/dao/admin_action_dao.py
+++ app/dao/admin_action_dao.py
@@ -1,5 +1,6 @@
 from emergency_alerts_utils.admin_action import ADMIN_STATUS_PENDING
 
+from app.dao.dao_utils import autocommit
 from app.models import AdminAction
 
 
@@ -15,6 +16,7 @@ def dao_get_all_admin_actions_by_user_id(user_id) -> list[AdminAction]:
     return AdminAction.query.filter_by(created_by_id=user_id).all()
 
 
+@autocommit
 def dao_delete_admin_action_by_id(action_id):
     """Delete an admin action - not just reject it (for tests)"""
-    return AdminAction.query.filter_by(id=action_id).delete()
+    return AdminAction.query.filter(AdminAction.id == action_id).delete()
