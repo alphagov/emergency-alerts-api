04efc2b33e8da90141cfd4a89560c230f352f205
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-05-28 16:14:46 +0100

    wip

--- app/celery/broadcast_message_tasks.py
+++ app/celery/broadcast_message_tasks.py
@@ -107,7 +107,7 @@ def check_event_makes_sense_in_sequence(broadcast_event, provider):
 
 @notify_celery.task(name="send-broadcast-event")
 def send_broadcast_event(broadcast_event_id):
-    current_app.logger.debug(f"Task 'send-broadcast-event' started for event id {broadcast_event_id}")
+    current_app.logger.info(f"Task 'send-broadcast-event' started for event id {broadcast_event_id}")
 
     broadcast_event = dao_get_broadcast_event_by_id(broadcast_event_id)
 
