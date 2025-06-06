878804b171c32b4f0b37718bccaa4db3abc9af79
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-06-06 09:55:52 +0100

    add logging

--- application.py
+++ application.py
@@ -1,9 +1,21 @@
 ##!/usr/bin/env python
+import os
+
 from __future__ import print_function
 
 from app import create_app
 from app.notify_api_flask_app import NotifyApiFlaskApp
 
 application = NotifyApiFlaskApp("app")
-
 create_app(application)
+
+service_name = os.environ.get("SERVICE", "unknown")
+application.logger.info(
+    f"{service_name.upper()} service Database configuration",
+    extra={
+        "python_module": __name__,
+        "sqlalchemy_engine_options": application.config["SQLALCHEMY_ENGINE_OPTIONS"],
+        "database_statement_timeout_ms": application.config["DATABASE_STATEMENT_TIMEOUT_MS"],
+        "sqlalchemy_database_uri": application.config["SQLALCHEMY_DATABASE_URI"],
+    },
+)
--- run_celery.py
+++ run_celery.py
@@ -5,10 +5,24 @@
 # prometheus will simply store the metrics in memory
 import prometheus_client  # noqa
 
+import os
+
 # notify_celery is referenced from manifest_delivery_base.yml, and cannot be removed
 from app import create_app, notify_celery  # noqa
 from app.notify_api_flask_app import NotifyApiFlaskApp
 
 application = NotifyApiFlaskApp("delivery")
 create_app(application)
+
+service_name = os.environ.get("SERVICE", "unknown")
+application.logger.info(
+    f"{service_name.upper()} Database configuration",
+    extra={
+        "python_module": __name__,
+        "sqlalchemy_engine_options": application.config["SQLALCHEMY_ENGINE_OPTIONS"],
+        "database_statement_timeout_ms": application.config["DATABASE_STATEMENT_TIMEOUT_MS"],
+        "sqlalchemy_database_uri": application.config["SQLALCHEMY_DATABASE_URI"],
+    },
+)
+
 application.app_context().push()
