7ca94509669262bb985d87b985a17a7e39b803ea
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-06-09 12:30:35 +0100

    testing

--- app/celery/broadcast_message_tasks.py
+++ app/celery/broadcast_message_tasks.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 
 from emergency_alerts_utils.xml.common import HEADLINE
 from flask import current_app
+from psycopg2 import OperationalError
 
 from app import cbc_proxy_client, db, notify_celery
 from app.clients.cbc_proxy import CBCProxyRetryableException
@@ -106,7 +107,14 @@ def check_event_makes_sense_in_sequence(broadcast_event, provider):
                 )
 
 
-@notify_celery.task(name="send-broadcast-event")
+@notify_celery.task(
+    name="send-broadcast-event",
+    bind=True,
+    autoretry_for=(OperationalError,),
+    retry_backoff=3,
+    retry_jitter=False,
+    max_retries=5,
+)
 def send_broadcast_event(broadcast_event_id):
     current_app.logger.info(
         f"Task 'send-broadcast-event' started for event id {broadcast_event_id}",
