eb92e218eaa7c2fe3ac788f05228e83d7f301c35
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-06-24 11:39:06 +0100

    Delete constants.py

--- app/celery/constants.py
+++
@@ -1,9 +0,0 @@
-class QueueNames(object):
-    PERIODIC = "periodic-tasks"
-    BROADCASTS = "broadcast-tasks"
-    GOVUK_ALERTS = "govuk-alerts"
-
-
-class TaskNames(object):
-    PUBLISH_GOVUK_ALERTS = "publish-govuk-alerts"
-    TRIGGER_GOVUK_HEALTHCHECK = "trigger-govuk-alerts-healthcheck"
