b8e60cdffbe594f3ceecdf7b29ba29d4b6a81cd5
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-07-31 16:35:47 +0100

    add flag to exclude alert from being rendered

---
+++ migrations/versions/0418_add_message_exclude_col.py
@@ -0,0 +1,23 @@
+"""
+
+Revision ID: 
+Revises: 0417_add_extra_content_cols
+Create Date: 2025-07-06 11:56:00
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = ""
+down_revision = "0417_add_extra_content_cols"
+
+
+def upgrade():
+    op.add_column("broadcast_message", sa.Column("exclude", sa.Boolean(), default=False))
+    op.execute("UPDATE broadcast_message SET exclude = false")
+    op.alter_column("broadcast_message", "exclude", nullable=False)
+
+
+def downgrade():
+    op.drop_column("broadcast_message", "exclude")
