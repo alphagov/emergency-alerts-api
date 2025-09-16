01319a8f8ff578d1f1bde84de84caf6b0dcaf560
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-09-16 12:24:20 +0100

    Adds migration version for adding postgis extension

---
+++ migrations/versions/0421_add_postgis_extension.py
@@ -0,0 +1,28 @@
+"""
+
+Revision ID: 0421_add_postgis_extension
+Revises: 0420_add_default_template_areas
+Create Date: 2025-09-10 15:51:00
+
+"""
+
+from alembic import op
+
+revision = "0421_add_postgis_extension"
+down_revision = "0420_add_default_template_areas"
+
+
+def upgrade():
+    op.execute(
+        """
+        CREATE EXTENSION IF NOT EXISTS postgis
+        """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+        DROP EXTENSION IF EXISTS postgis
+        """
+    )
