ef4403fa0779348741b21f68d8ae632382b2c54d
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-11-12 16:58:46 +0000

    Log on run_celery

--- run_celery.py
+++ run_celery.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 # notify_celery must be imported here
+import logging
+
 from app import create_app, notify_celery  # noqa
 from app.notify_api_flask_app import NotifyApiFlaskApp
 
 application = NotifyApiFlaskApp("delivery")
 create_app(application)
 application.app_context().push()
+
+logger = logging.getLogger(__name__)
+logger.info("run_celery init")
