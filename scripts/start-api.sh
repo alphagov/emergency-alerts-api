e34e56bab6463a093eb8f1e15663bdc355328c1a
Adam-C-Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2026-01-13 20:02:36 +0000

    Enable multiple backup buckets

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -1,7 +1,7 @@
-#! /bin/sh
+#!/usr/bin/env bash
 echo "Start script executing for api.."
 
-function put_metric_data(){
+put_metric_data(){
     if [[ $1 != "Backups" && $1 != "Migrations" ]]; then
         echo "A namespace is required (Backups || Migrations)."
         exit 1;
@@ -26,34 +26,39 @@ function put_metric_data(){
     dimension="PipelineRunId=$PIPELINE_RUN_ID,StartTime=$START_TIME,Status=$2"
     simple_dimension="Status=$2" # Just the status dimension; for CloudWatch alarming purposes
 
+    # We have multiple backup buckets, so a third argument can be passed to
+    # reflect the particular bucket the backup was pushed to.
+    if [[ -z $3 && $1 == "Backups" ]]; then
+        simple_dimension="$simple_dimension,Bucket=$3"
+    fi
 
     echo "Putting metric $1[$SERVICE_ACTION] with dimension: $dimension"
     aws cloudwatch put-metric-data \
-        --namespace $1 \
-        --metric-name $SERVICE_ACTION \
-        --dimensions $dimension \
+        --namespace "$1" \
+        --metric-name "$SERVICE_ACTION" \
+        --dimensions "$dimension" \
         --value 1 \
-        --timestamp $(date -u +"%Y-%m-%dT%H:%M:%S.000Z") \
-        --region ${AWS_REGION:-eu-west-2}
+        --timestamp "$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")" \
+        --region "${AWS_REGION:-eu-west-2}"
 
     aws cloudwatch put-metric-data \
-        --namespace $1 \
-        --metric-name $SERVICE_ACTION \
-        --dimensions $simple_dimension \
+        --namespace "$1" \
+        --metric-name "$SERVICE_ACTION" \
+        --dimensions "$simple_dimension" \
         --value 1 \
-        --timestamp $(date -u +"%Y-%m-%dT%H:%M:%S.000Z") \
-        --region ${AWS_REGION:-eu-west-2}
+        --timestamp "$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")" \
+        --region "${AWS_REGION:-eu-west-2}"
 }
 
-function run_db_migrations(){
-    cd $DIR_API;
+run_db_migrations(){
+    cd "$DIR_API";
     . $VENV_API/bin/activate
 
     local head=$(flask db heads | tail -1)
     local current=$(flask db current | tail -1)
 
     # Checking if the database upgrade has been previously been completed to avoid running multiple times.
-    if [[ $head != $current ]]; then
+    if [[ $head != "$current" ]]; then
         echo "Run DB migration"
         if flask db upgrade; then
             put_metric_data "Migrations" "success"
@@ -66,7 +71,7 @@ function run_db_migrations(){
     fi
 }
 
-function backup_database(){
+backup_database(){
     if [[ -z $RDS_HOST  ]]; then
         echo "RDS_HOST is not provided but is required."
         put_metric_data "Backups" "failure"
@@ -85,8 +90,8 @@ function backup_database(){
         exit 1;
     fi
 
-    if [[ -z $BACKUP_BUCKET_NAME ]]; then
-        echo "BACKUP_BUCKET_NAME is not provided but is required."
+    if [[ -z $BACKUP_BUCKET_NAMES ]]; then
+        echo "BACKUP_BUCKET_NAMES is not provided but is required."
         put_metric_data "Backups" "failure"
         exit 1;
     fi
@@ -100,16 +105,16 @@ function backup_database(){
     # Must be in /eas as it's owned by easuser
     SQL_FILENAME_SUFFIX=$ENVIRONMENT-$(date -u +"%Y-%m-%d-%H-%M-%S").sql
     SQL_FILENAME=/eas/$SQL_FILENAME_SUFFIX
-    rm -f $SQL_FILENAME
+    rm -f "$SQL_FILENAME"
 
     echo "Running pg_dump to $SQL_FILENAME"
 
     # To exclude table use: --exclude-table TABLE_NAME
-    PGPASSWORD=$MASTER_PASSWORD pg_dump -h $RDS_HOST -p $RDS_PORT -U $MASTER_USERNAME \
-        --file $SQL_FILENAME \
-        $DATABASE
+    PGPASSWORD=$MASTER_PASSWORD pg_dump -h "$RDS_HOST" -p "$RDS_PORT" -U "$MASTER_USERNAME" \
+        --file "$SQL_FILENAME" \
+        "$DATABASE"
 
-    if [ $(cat $SQL_FILENAME | grep "PostgreSQL database dump" | wc -l) -lt 2 ]; then
+    if [ "$(cat "$SQL_FILENAME" | grep "PostgreSQL database dump" | wc -l)" -lt 2 ]; then
         echo "There was an issue creating the backup.";
         put_metric_data "Backups" "failure"
         exit 1;
@@ -117,40 +122,44 @@ function backup_database(){
 
     ARCHIVE_FILENAME_SUFFIX=$SQL_FILENAME_SUFFIX.tar
     ARCHIVE_FILENAME=/eas/$ARCHIVE_FILENAME_SUFFIX
-    tar -cf $ARCHIVE_FILENAME $SQL_FILENAME
+    tar -cf "$ARCHIVE_FILENAME" "$SQL_FILENAME"
 
     echo "Created $ARCHIVE_FILENAME"
-    ls -lh $ARCHIVE_FILENAME
+    ls -lh "$ARCHIVE_FILENAME"
 
     # Upload the backup to S3
-    if aws s3api put-object \
-        --bucket $BACKUP_BUCKET_NAME \
-        --key $ENVIRONMENT/$ARCHIVE_FILENAME_SUFFIX \
-        --body $ARCHIVE_FILENAME;
-    then
-        echo "Bucket name: $BACKUP_BUCKET_NAME"
-        echo "Bucket key: $ENVIRONMENT/$ARCHIVE_FILENAME_SUFFIX"
-        echo "Backup created successfully."
+    IFS=',' read -ra buckets <<< "$BACKUP_BUCKET_NAMES"
 
-        put_metric_data "Backups" "success"
-    else
-        echo "Error uploading backup to S3";
-        put_metric_data "Backups" "failure"
-        exit 1;
-    fi
+    for bucket in "${buckets[@]}"; do
+        if aws s3api put-object \
+            --bucket "$bucket" \
+            --key "$ENVIRONMENT/$ARCHIVE_FILENAME_SUFFIX" \
+            --body "$ARCHIVE_FILENAME";
+        then
+            echo "Bucket name: $bucket"
+            echo "Bucket key: $ENVIRONMENT/$ARCHIVE_FILENAME_SUFFIX"
+            echo "Backup created successfully."
+
+            put_metric_data "Backups" "success" "$bucket"
+        else
+            echo "Error uploading backup to S3";
+            put_metric_data "Backups" "failure" "$bucket"
+            exit 1;
+        fi
+    done
 }
 
-function configure_container_role(){
+configure_container_role(){
     aws configure set default.region ${AWS_REGION:-eu-west-2}
 }
 
-function run_celery(){
-    cd $DIR_API;
+run_celery(){
+    cd "$DIR_API";
     . $VENV_API/bin/activate && make run-celery-api &
 }
 
-function run_api(){
-    cd $DIR_API;
+run_api(){
+    cd "$DIR_API";
     . $VENV_API/bin/activate && opentelemetry-instrument flask run -p 6011 --host=0.0.0.0
 }
 
