8e47d68ddfb92e3e3f3e6429871708c28d8b6630
Dave Johnson <dave.johnson@digital.cabinet-office.gov.uk>
2026-02-13 15:13:26 +0000

    Fix flake8 errors.

--- app/v2/errors.py
+++ app/v2/errors.py
@@ -14,8 +14,9 @@ class TooManyRequestsError(InvalidRequest):
     status_code = 429
     message_template = "Exceeded send limits ({}) for today"
 
-    def __init__(self, sending_limit):
-        super().__init__(sending_limit)
+    def __init__(self, sending_limit, status_code=429):
+        super().__init__(sending_limit, status_code)
+        self.status_code = status_code
         self.message = self.message_template.format(sending_limit)
 
 
@@ -36,20 +37,18 @@ class RateLimitError(InvalidRequest):
 class BadRequestError(InvalidRequest):
     message = "An error occurred"
 
-    def __init__(self, fields=None, message=None, status_code=400):
-        super().__init__(fields, message, status_code)
+    def __init__(self, message=None, status_code=400):
+        super().__init__(message, status_code)
         self.status_code = status_code
-        self.fields = fields or []
         self.message = message if message else self.message
 
 
 class ValidationError(InvalidRequest):
     message = "Your notification has failed validation"
 
-    def __init__(self, fields=None, message=None, status_code=400):
-        super().__init__(fields, message, status_code)
+    def __init__(self, message=None, status_code=400):
+        super().__init__(message, status_code)
         self.status_code = status_code
-        self.fields = fields or []
         self.message = message if message else self.message
 
 
