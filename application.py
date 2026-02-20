7d70c13089ba433a21fd07c2573d6e3bc281be09
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-02-20 17:20:42 +0000

    Instrument on app start

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 ##!/usr/bin/env python
+
+from opentelemetry.instrumentation.auto_instrumentation.sitecustomize import (
+    initialize,
+)
+
 import app
 from app.notify_api_flask_app import NotifyApiFlaskApp
 
+initialize()  # performs the same auto-instrumentation as the CLI
 application = NotifyApiFlaskApp("app")
 app.create_app(application)
