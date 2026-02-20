8648cf13b9a9656f7a79d3694159186812167fb6
Alec Ashmore <alec.ashmore@digital.cabinet-office.gov.uk>
2026-02-20 13:29:35 +0000

    Remove Mehmet from mno contact

--- app/celery/log_ingest_tasks.py
+++ app/celery/log_ingest_tasks.py
@@ -107,11 +107,7 @@ def _get_mno_contact_emails(provider_id):
     # TODO: Implement MNO contact information after testing is completed.
 
     # Placeholder example:
-    return [
-        "alec.ashmore@digital.cabinet-office.gov.uk",
-        "simon.sorrell@digital.cabinet-office.gov.uk",
-        "mehmet.omer@digital.cabinet-office.gov.uk",
-    ]
+    return ["alec.ashmore@digital.cabinet-office.gov.uk", "simon.sorrell@digital.cabinet-office.gov.uk"]
 
 
 def _invoke_log_upload_lambda(lambda_name, payload):
