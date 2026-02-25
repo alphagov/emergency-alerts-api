bb6b409b572f9da714d2beec271535456a29640d
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-02-25 14:34:52 +0000

    Stub tasks

--- app/tasks/stub_tasks.py
+++ app/tasks/stub_tasks.py
@@ -7,5 +7,16 @@ from app import dramatiq
 
 
 @dramatiq.actor(actor_name=TaskNames.PUBLISH_GOVUK_ALERTS, queue_name=QueueNames.GOVUK_ALERTS)
-def publish_govuk_alerts():
-    pass
+def publish_govuk_alerts(*args, **kwargs):
+    raise NotImplementedError(
+        f"Attempted to run {TaskNames.PUBLISH_GOVUK_ALERTS} but this is the "
+        "API worker - not govuk. Either this task was sent on the wrong queue or we're consuming the wrong queue."
+    )
+
+
+@dramatiq.actor(actor_name=TaskNames.TRIGGER_GOVUK_HEALTHCHECK, queue_name=QueueNames.GOVUK_ALERTS)
+def trigger_govuk_healthcheck(*args, **kwargs):
+    raise NotImplementedError(
+        f"Attempted to run {TaskNames.TRIGGER_GOVUK_HEALTHCHECK} but this is the "
+        "API worker - not govuk. Either this task was sent on the wrong queue or we're consuming the wrong queue."
+    )
