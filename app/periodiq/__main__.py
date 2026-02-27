22f5b49debafdff284c51d4b8171584b9c453da8
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-02-27 10:32:14 +0000

    Add comment

--- app/periodiq/__main__.py
+++ app/periodiq/__main__.py
@@ -1,4 +1,7 @@
 import periodiq
 
+# We can't use the Periodiq CLI as then the import order is wrong for OpenTelemetry
+# (our instrumentation can't override the __main__ module from Periodiq's CLI)
+
 if "__main__" == __name__:
     periodiq.entrypoint()
