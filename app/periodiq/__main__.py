91985978926d24afd77fc68b4b43badabc2c2a24
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-03-18 16:31:11 +0000

    Patch out logging in Periodiq

--- app/periodiq/__main__.py
+++ app/periodiq/__main__.py
@@ -1,7 +1,16 @@
+import logging
+
 import periodiq
 
 # We can't use the Periodiq CLI as then the import order is wrong for OpenTelemetry
 # (our instrumentation can't override the __main__ module from Periodiq's CLI)
 
 if "__main__" == __name__:
+    # Hack: The periodiq entrypoint sets up a logger but our app does that
+    # Let's just patch that out
+    def noop(**kwargs):
+        pass
+
+    logging.basicConfig = noop
+
     periodiq.entrypoint()
