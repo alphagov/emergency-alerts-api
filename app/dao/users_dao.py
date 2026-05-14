4026e42169520fd75b16b53c0fe50020552cc2cf
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2026-05-14 16:10:23 +0000

    Remove manage_setting references and remove the check when archiving user

--- app/dao/users_dao.py
+++ app/dao/users_dao.py
@@ -175,7 +175,7 @@ def get_user_and_accounts(user_id):
 @autocommit
 def dao_archive_user(user):
     if not user_can_be_archived(user):
-        msg = "User can’t be removed from a service - check all services have another team member with manage_settings"
+        msg = "User can’t be removed from a service - check all services have other team members"
         raise InvalidRequest(msg, 400)
 
     permission_dao.remove_user_service_permissions_for_all_services(user)
@@ -206,10 +206,6 @@ def user_can_be_archived(user):
         other_active_users = [x for x in service.users if x.state == "active" and x != user]
 
         if not other_active_users:
-            return False
-
-        if not any("manage_settings" in user.get_permissions(service.id) for user in other_active_users):
-            # no-one else has manage settings
             return False
 
     return True
