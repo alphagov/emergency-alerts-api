4c7ca4832a1441796234261cbbe6dd432e780355
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2026-06-03 14:18:33 +0000

    Move files

---
+++ scripts/add-pop-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -h "$RDS_HOST" -p "$RDS_PORT" <<-EOSQL
+	\COPY populations (id, geometry, density) FROM './scripts/population_data.csv' CSV HEADER;
+EOSQL
