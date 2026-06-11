6265fe969ac82a1350faa5686da25a3421c7644b
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2026-06-11 22:59:31 +0100

    Adds WKT validation of area before estimating population

--- app/populations/rest.py
+++ app/populations/rest.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
 
-from app.dao.populations_dao import dao_estimate_population_for_polygon
+from app.dao.populations_dao import dao_estimate_population_for_area
 from app.errors import register_errors
+from shapely import MultiPolygon, Polygon, wkt
 
 populations_blueprint = Blueprint(
     "populations",
@@ -16,4 +18,23 @@ register_errors(populations_blueprint)
 def get_population_estimate_for_area():
     # get alert area population data based on area posted
     data = request.get_json()
-    return jsonify(dao_estimate_population_for_polygon(data.get("area")))
+    area = data.get("areas")
+    validate_wkt_area(area)
+    return jsonify(dao_estimate_population_for_area(area))
+
+def validate_wkt_area(area):
+    # Firstly check string is valid WKT
+    try:
+        geom = wkt.loads(area)
+    except Exception:
+        raise ValidationError("Invalid WKT string")
+
+    # Checking area is either Polygon or MultiPolygon
+    if not isinstance(geom, (Polygon, MultiPolygon)):
+        raise ValidationError("Area must be a Polygon or MultiPolygon")
+
+    # Checking area has valid shape
+    if not geom.is_valid:
+        raise ValidationError("Provided WKT area is not valid")
+
+
