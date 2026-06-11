8e8e1a2751fda288e6c31c7097a80b682ff03d42
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2026-06-11 22:58:51 +0100

    Updating the population estimate query

--- app/dao/populations_dao.py
+++ app/dao/populations_dao.py
@@ -1,9 +1,11 @@
 from app import db
 
 
-def dao_estimate_population_for_polygon(polygon):
-    query = f"""WITH proposed_polygon AS (
-                SELECT ST_GeomFromText('{polygon}', 4326) AS geom
+def dao_estimate_population_for_area(polygon):
+    # Estimates population by calculating the intersection of the area
+    # with areas with known population counts
+    query = """WITH proposed_polygon AS (
+                SELECT ST_GeomFromText(:polygon, 4326) AS geom
             )
             SELECT
                 SUM(
@@ -12,5 +14,5 @@ def dao_estimate_population_for_polygon(polygon):
             FROM populations t, proposed_polygon
             WHERE ST_Intersects(t.geometry, proposed_polygon.geom)
         """
-    result = db.session.execute(query).fetchone()
+    result = db.session.execute(query, {"polygon": polygon}).fetchone()
     return result[0] or 0
